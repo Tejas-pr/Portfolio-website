e77d829831ad6363441f5ec26f6bf9e004e117e5
Tejas-pr <Tejas.teju02@gmail.com>
2025-02-25 22:05:27 +0530

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import image1 from "../image/img1.png";
 import image2 from "../image/img2.png";
 import image3 from "../image/img3.png";
 import image4 from "../image/img4.png";
+import image5 from "../image/img5.png";
 
 const ScrollReveal = ({ children }) => {
   return (
@@ -21,12 +22,12 @@ const ScrollReveal = ({ children }) => {
 const ProjectCard = ({ project }) => {
   return (
     <ScrollReveal>
-      <div className="flex flex-col items-center gap-8 md:flex-row md:gap-20 rounded-lg shadow-sm shadow-slate-800 p-8">
+      <div className="flex flex-col items-center gap-8 md:flex-row md:gap-10 rounded-lg shadow-sm shadow-slate-800 p-8">
         <a href={project.liveLink}>
           <img
             src={project.image}
             alt={project.title}
-            className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[400px]"
+            className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[500px]"
           />
         </a>
         <div className="flex flex-col gap-5">
@@ -74,6 +75,28 @@ const ProjectCard = ({ project }) => {
 const Projects = () => {
   const projectsData = [
     {
+      image: image4,
+      title: "Space Landing Page",
+      description:
+        "A visually stunning Space Landing Page built using React, TypeScript, and Tailwind CSS. This project creates an engaging and modern UI to showcase space-related content.",
+      technologies: ["React", "TailwindCSS", "Typescript"],
+      githubLink: "https://github.com/Tejas-pr/Space-ts",
+      liveLink: "https://space-ts-landingpage.vercel.app/",
+    },
+    {
+      image: image5,
+      title: "Paytm Clone",
+      description:
+        "This project is a Paytm clone built using Reactjs, Tailwindcss, Node.js, Express, and MongoDB with Mongoose. The primary goal is to understand and implement user-to-user transactions using MongoDB sessions to ensure atomicity and consistency.",
+      technologies: [
+        "ReactJs",
+        "MERN",
+        "TailwindCSS",
+      ],
+      githubLink: "https://github.com/Tejas-pr/paytm",
+      liveLink: "https://github.com/Tejas-pr/paytm",
+    },
+    {
       image: image2,
       title: "AI PDF Chatbot Fullstack",
       description:
@@ -92,13 +115,13 @@ const Projects = () => {
       liveLink: "https://ai-pdf-chatbot-fullstack.vercel.app/",
     },
     {
-      image: image4,
-      title: "Space Landing Page",
+      image: image1,
+      title: "Shoptopia E-commerce Platform",
       description:
-        "A visually stunning Space Landing Page built using React, TypeScript, and Tailwind CSS. This project creates an engaging and modern UI to showcase space-related content.",
-      technologies: ["React", "TailwindCSS", "Typescript"],
-      githubLink: "https://github.com/Tejas-pr/Space-ts",
-      liveLink: "https://space-ts-landingpage.vercel.app/",
+        "A Shoptopia is an e-commerce platform where users can browse, add, and view products.An admin panel is available for managing products efficiently",
+      technologies: ["MERN", "TailwindCSS", "Framer-Motion", "GSAP"],
+      githubLink: "https://github.com/Tejas-pr/Shoptopia",
+      liveLink: "https://shoptopia-frontend.vercel.app/",
     },
     {
       image: image3,
@@ -111,18 +134,9 @@ const Projects = () => {
         "Framer-Motion",
         "Chatjs",
         "OpenWeatherMap API",
-      ],
+      ],  
       githubLink: "https://github.com/Tejas-pr/WeatherSphere",
       liveLink: "https://weather-sphere-frontend.vercel.app/",
-    },
-    {
-      image: image1,
-      title: "Shoptopia E-commerce Platform",
-      description:
-        "A Shoptopia is an e-commerce platform where users can browse, add, and view products.An admin panel is available for managing products efficiently",
-      technologies: ["MERN", "TailwindCSS", "Framer-Motion", "GSAP"],
-      githubLink: "https://github.com/Tejas-pr/Shoptopia",
-      liveLink: "https://shoptopia-frontend.vercel.app/",
     },
   ];
 
